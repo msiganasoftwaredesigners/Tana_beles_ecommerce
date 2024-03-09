d19a738eae95062e19fe0d9a46a45995a9c0451c
melaku <melakushiferaw05@gmail.com>
2024-03-09 21:31:40 +0300

    register telebirr model to admin

--- telebirrpay/admin.py
+++ telebirrpay/admin.py
@@ -1,3 +1,7 @@
+# admin.py
 from django.contrib import admin
+from .models import PaymentNotification
 
-# Register your models here.
+@admin.register(PaymentNotification)
+class PaymentNotificationAdmin(admin.ModelAdmin):
+    list_display = ['msisdn', 'out_trade_no', 'total_amount', 'trade_date', 'trade_no', 'trade_status', 'transaction_no']
